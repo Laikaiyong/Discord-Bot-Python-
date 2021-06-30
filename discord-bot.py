854b7843b5c17c1063b27e16517e827f27b3b540
Laikaiyong <vandycklai@gmail.com>
2021-06-30 10:19:13 +0800

    Improve 01 welcome feature

--- discord-bot.py
+++ discord-bot.py
@@ -93,12 +93,12 @@ async def on_member_join(member):
     member_im = Image.open(requests.get(member.avatar_url, stream=True).raw)
     member_im = member_im.resize((310, 300), Image.LANCZOS)
 
-    img.paste(member_im, (int(1150/4), int(669/4)))
+    img.paste(member_im, (int(1150/3)+15, int(669/4)))
     msg = "Welcome " + member.name + " (Member " + str(member_count) + ")"
     draw = ImageDraw.Draw(img)
-    fox_n_cat = ImageFont.truetype("Fox & Cat.ttf", 35)
-    w, h = draw.textsize(msg, font=fox_n_cat)
-    draw.text(((1150-w)/2, (669-h)/8*7), msg, fill="black", font=fox_n_cat)
+    chunk_five = ImageFont.truetype("Chunk Five Print.otf", 40)
+    w, h = draw.textsize(msg, font=chunk_five)
+    draw.text(((1150-w)/2, (669-h)/8*7), msg, fill="black", font=chunk_five)
     img.save("new.png", "PNG")
 
     await channel.send(f"Welcome to the server {member.mention}! :partying_face:\n:one: Check out <#{852088286922801193}> to redeem membership :white_check_mark:\n:two: Stay updated on events in <#{856440780164169738}> :fireworks:\n:three: Customize your unique role in <#{852111666716213258}> :scroll:\n:four: Get useful resources in <#{852101645836091472}> on your developing journey :person_climbing:\n", file=discord.File("new.png"))
