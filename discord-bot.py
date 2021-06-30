18d82afe264d95cf6a48804290963b668ea92baf
Laikaiyong <vandycklai@gmail.com>
2021-06-30 10:11:15 +0800

    Debug 12 welcome feature

--- discord-bot.py
+++ discord-bot.py
@@ -91,17 +91,14 @@ async def on_member_join(member):
     img = img.copy()
 
     member_im = Image.open(requests.get(member.avatar_url, stream=True).raw)
-    mask = Image.open('mask.png'). convert('L')
     member_im = member_im.resize((310, 300), Image.LANCZOS)
-    member_im = ImageOps.fit(member_im, mask.size, centering=(0.5, 0.5))
-    member_im.putalpha(mask)
 
-    img.paste(member_im, (int(1150/3), int(669/3)))
-    msg = "Welcome" + member.name + " (Member " + str(member_count) + ")"
+    img.paste(member_im, (int(1150/4), int(669/4)))
+    msg = "Welcome " + member.name + " (Member " + str(member_count) + ")"
     draw = ImageDraw.Draw(img)
-    magistra = ImageFont.truetype("MagicstraDemoRegular.ttf", 20)
-    w, h = draw.textsize(msg, font=magistra)
-    draw.text(((1150-w)/2, (669-h)/3*2), msg, fill="black", font=magistra)
+    fox_n_cat = ImageFont.truetype("Fox & Cat.ttf", 35)
+    w, h = draw.textsize(msg, font=fox_n_cat)
+    draw.text(((1150-w)/2, (669-h)/8*7), msg, fill="black", font=fox_n_cat)
     img.save("new.png", "PNG")
 
     await channel.send(f"Welcome to the server {member.mention}! :partying_face:\n:one: Check out <#{852088286922801193}> to redeem membership :white_check_mark:\n:two: Stay updated on events in <#{856440780164169738}> :fireworks:\n:three: Customize your unique role in <#{852111666716213258}> :scroll:\n:four: Get useful resources in <#{852101645836091472}> on your developing journey :person_climbing:\n", file=discord.File("new.png"))
