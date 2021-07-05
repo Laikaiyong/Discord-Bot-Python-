3d31deafc596e6e7af39dd11c21b0bc7a8fbc691
Laikaiyong <vandycklai@gmail.com>
2021-07-05 21:11:47 +0800

    Debug xN times

--- discord-bot.py
+++ discord-bot.py
@@ -40,107 +40,100 @@ cluster = MongoClient(os.environ.get('LINK'))
 levelling = cluster["KYGorGorCluster"]["Levelling"]
 
 
-class levelsys(commands.Cog):
+@commands.Cog.listener()
+async def on_message(message):
+    if message.channel.id in talk_channels:
+        stats = levelling.find_one({"id": message.author.id})
 
-    def __init__(self, client):
-        self.client = client
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("ready")
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.channel.id in talk_channels:
-            stats = levelling.find_one({"id": message.author.id})
-
-            if not message.author.bot:
-                if stats is None:
-                    new_user = {"id": message.author.id, "xp": 10}
-                    levelling.insert_one(new_user)
-                else:
-                    xp = stats["xp"] + 5
-                    levelling.update_one({"id": message.author.id}, {
-                                         "$set": {"xp": xp}})
-                    lvl = 0
-                    while True:
-                        if xp < ((50*(lvl**2))+(50*(lvl-1))):
-                            break
-                        lvl += 1
-                    xp -= ((50*(lvl**2))+(50*(lvl-1)))
-                    if xp == 0:
-                        await message.channel.send(f"{message.author.mention} had reached **Level {lvl}**, keep up the momentum")
-                        for i in range(len(level)):
-                            if lvl == level_num[i]:
-                                await message.author.add_roles(discord.utils.get(message.author.guild.roles, name=i))
-
-                                embed = discord.Embed(
-                                    description=f"{message.author.mention}, you earned **{i}** role!"
-                                )
-                                embed.set_thumbnail(
-                                    url=message.author.avatar_url)
-                                embed.set_footer(
-                                    text='\u200b Behold, the wisdom of activeness')
-                                await message.channel.send(embed=embed)
-
-    @commands.command(alias='r')
-    async def rank(self, ctx):
-        if ctx.channel.id in bot_channel:
-            stats = levelling.find_one({"id": ctx.author.id})
-
+        if not message.author.bot:
             if stats is None:
-                embed = discord.Embed(
-                    description="You haven't send any messages, send 1 and try again \U0001F60F")
-
+                new_user = {"id": message.author.id, "xp": 10}
+                levelling.insert_one(new_user)
             else:
-                xp = stats["xp"]
+                xp = stats["xp"] + 5
+                levelling.update_one({"id": message.author.id}, {
+                    "$set": {"xp": xp}})
                 lvl = 0
-                rank = 0
                 while True:
                     if xp < ((50*(lvl**2))+(50*(lvl-1))):
                         break
                     lvl += 1
-
                 xp -= ((50*(lvl**2))+(50*(lvl-1)))
+                if xp == 0:
+                    await message.channel.send(f"{message.author.mention} had reached **Level {lvl}**, keep up the momentum")
+                    for i in range(len(level)):
+                        if lvl == level_num[i]:
+                            await message.author.add_roles(discord.utils.get(message.author.guild.roles, name=i))
 
-                boxes = int((xp/(200*((1/2) * lvl)))*20)
-                rankings = levelling.find().sort("xp", -1)
-                for x in rankings:
-                    rank += 1
-                    if stats["id"] == x["id"]:
-                        break
+                            embed = discord.Embed(
+                                description=f"{message.author.mention}, you earned **{i}** role!"
+                            )
+                            embed.set_thumbnail(
+                                url=message.author.avatar_url)
+                            embed.set_footer(
+                                text='\u200b Behold, the wisdom of activeness')
+                            await message.channel.send(embed=embed)
 
-                embed = discord.Embed(
-                    title="{}'s level stats".format(ctx.author.name))
-                embed.add_field(
-                    name="Name", value=ctx.author.mention, inline=True)
-                embed.add_field(
-                    name="XP", value=f"{xp}/{int(200 *((1/2) * lvl))}", inline=True)
-                embed.add_field(
-                    name="Rank", value=f"{rank}/{ctx.guild.member_count}", inline=True)
-                embed.add_field(name="Progress Bar [lvl]", value=boxes * ":blue_square:" + (
-                    20 - boxes) * ":white_large_square:", inline=True)
-                embed.set_thumbnail(url=ctx.author.avatar_url)
-                await ctx.channel.send(embed=embed)
 
-    @commands.command(alias='ldb')
-    async def leaderboard(self, ctx):
-        if ctx.channel.id in bot_channel:
-            rankings = levelling.find().sort("xp", -1)
-            i = 1
-            embed = discord.Embed(title="Active Ranking:")
-            for x in rankings:
-                try:
-                    temp = ctx.guild.get_member(x["id"])
-                    temp_xp = x["xp"]
-                    embed.add_field(
-                        name=f"{i}: {temp.name}", value=f"total xp: {temp_xp}", inline=False)
-                    i += 1
-                except:
-                    pass
-                if i == 11:
+@commands.command(aliases=['r'])
+async def rank(ctx):
+    if ctx.channel.id in bot_channel:
+        stats = levelling.find_one({"id": ctx.author.id})
+
+        if stats is None:
+            embed = discord.Embed(
+                description="You haven't send any messages, send 1 and try again \U0001F60F")
+
+        else:
+            xp = stats["xp"]
+            lvl = 0
+            rank = 0
+            while True:
+                if xp < ((50*(lvl**2))+(50*(lvl-1))):
                     break
+                lvl += 1
+
+            xp -= ((50*(lvl**2))+(50*(lvl-1)))
+
+            boxes = int((xp/(200*((1/2) * lvl)))*20)
+            rankings = levelling.find().sort("xp", -1)
+            for x in rankings:
+                rank += 1
+                if stats["id"] == x["id"]:
+                    break
+
+            embed = discord.Embed(
+                title="{}'s level stats".format(ctx.author.name))
+            embed.add_field(
+                name="Name", value=ctx.author.mention, inline=True)
+            embed.add_field(
+                name="XP", value=f"{xp}/{int(200 *((1/2) * lvl))}", inline=True)
+            embed.add_field(
+                name="Rank", value=f"{rank}/{ctx.guild.member_count}", inline=True)
+            embed.add_field(name="Progress Bar [lvl]", value=boxes * ":blue_square:" + (
+                20 - boxes) * ":white_large_square:", inline=True)
+            embed.set_thumbnail(url=ctx.author.avatar_url)
             await ctx.channel.send(embed=embed)
+
+
+@commands.command(aliases=['ldb'])
+async def leaderboard(ctx):
+    if ctx.channel.id in bot_channel:
+        rankings = levelling.find().sort("xp", -1)
+        i = 1
+        embed = discord.Embed(title="Active Ranking:")
+        for x in rankings:
+            try:
+                temp = ctx.guild.get_member(x["id"])
+                temp_xp = x["xp"]
+                embed.add_field(
+                    name=f"{i}: {temp.name}", value=f"total xp: {temp_xp}", inline=False)
+                i += 1
+            except:
+                pass
+            if i == 11:
+                break
+        await ctx.channel.send(embed=embed)
 
 
 # Join a new server and set prefix
@@ -663,5 +656,4 @@ async def place_error(ctx, error):
         await ctx.send("Please make sure to enter an integer.")
 
 
-client.add_cog(levelsys(client))
 client.run(os.environ.get('TOKEN'))
